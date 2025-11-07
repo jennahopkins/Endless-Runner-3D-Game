cd9759b2f967887f98a2d0853afc7a4b13a6c49c
jennahopkins <jhopkins2@willamette.edu>
2025-11-06 16:50:06 -0800

    fixed filing issue

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] GameManager gameManager;
+    [SerializeField] HighScoreManager highScoreManager;
     [SerializeField] TMP_Text scoreText;
 
     int score = 0;
 
-    public void IncreaseScore(int amount) 
+    public void IncreaseScore(int amount)
     {
         if (gameManager.GameOver) return;
 
         score += amount;
         scoreText.text = score.ToString();
+
+        if (score > highScoreManager.GetHighScore())
+        {
+            highScoreManager.SetText(score);
+        }
+    }
+    
+    public int GetScore()
+    {
+        return score;
     }
 }
